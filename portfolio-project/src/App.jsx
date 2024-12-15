9455db44440fda4f65fd5173e32d15d0ffa79e7f
Youssef Mashaly <metwally.y9@gmail.com>
2024-12-14 21:44:45 -0500

    resume button

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  return (
-    <>hello world</>
-  )
+    const openPDF = () => {
+        window.open('/portfolio/resume.pdf', '_blank');
+    };
+
+    return (
+        <div className="App">
+            <button className="resume-button" onClick={openPDF}>
+                Resume
+            </button>
+        </div>
+    );
 }
 
-export default App
+export default App;
