6434f32ba9825884e0fd656ed340f233206a73d8
Youssef Mashaly <metwally.y9@gmail.com>
2024-12-15 05:03:16 -0500

    adding some icons

--- portfolio-project/src/components/Home.jsx
+++ portfolio-project/src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaFilePdf } from 'react-icons/fa';
 import './Home.css';
+import { GoProjectRoadmap } from "react-icons/go";
 
 const Home = () => {
     const navigate = useNavigate();
@@ -12,9 +14,11 @@ const Home = () => {
     return (
         <div className="App">
             <button className="resume-button" onClick={openPDF}>
+                <FaFilePdf className="icon" />
                 Resume
             </button>
             <button className="projects-button" onClick={() => navigate('/projects')}>
+                <GoProjectRoadmap className="icon" />
                 Projects
             </button>
         </div>
